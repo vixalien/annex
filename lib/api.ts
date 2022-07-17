5b26fa07d25f967ce557a8cc187bf25763a00daf
Angelo Verlain <geoangercola@gmail.com>
2022-07-17 03:25:07 +0200

    remove debug data

--- lib/api.ts
+++ lib/api.ts
@@ -46,68 +46,6 @@ export interface Comment {
   rating?: number;
 }
 
-// extension info url
-// https://extensions.gnome.org/extension-info/?uuid=gsconnect@andyholmes.github.io
-
-const sampleExtension: Extension = {
-  "uuid": "gsconnect@andyholmes.github.io",
-  "name": "GSConnect",
-  "creator": "dlandau",
-  "creator_url": "/accounts/profile/dlandau",
-  "pk": 1319,
-  "description":
-    "GSConnect is a complete implementation of KDE Connect especially for GNOME Shell with Nautilus, Chrome and Firefox integration. It does not rely on the KDE Connect desktop application and will not work with it installed.\n\nKDE Connect allows devices to securely share content like notifications or files and other features like SMS messaging and remote control. The KDE Connect team has applications for Linux, BSD, Android, Sailfish and Windows.\n\nPlease report issues on Github!",
-  "link": "/extension/1319/gsconnect/",
-  "icon": "/extension-data/icons/icon_1319_4X2QAtk.png",
-  "screenshot": "/ex/extension-info/tension-data/screenshots/screenshot_1319_1NByhZ8.png",
-  "shell_version_map": {
-    "40": {
-      "pk": 25303,
-      "version": 47,
-    },
-    "41": {
-      "pk": 30306,
-      "version": 49,
-    },
-    "42": {
-      "pk": 30321,
-      "version": 50,
-    },
-    "3.24": {
-      "pk": 8326,
-      "version": 12,
-    },
-    "3.26": {
-      "pk": 8326,
-      "version": 12,
-    },
-    "3.28": {
-      "pk": 12823,
-      "version": 28,
-    },
-    "3.30": {
-      "pk": 12823,
-      "version": 28,
-    },
-    "3.32": {
-      "pk": 12823,
-      "version": 28,
-    },
-    "3.34": {
-      "pk": 14243,
-      "version": 33,
-    },
-    "3.36": {
-      "pk": 30336,
-      "version": 53,
-    },
-    "3.38": {
-      "pk": 30333,
-      "version": 51,
-    },
-  },
-};
-
 const normalizeLink = (link: string): string => {
   if (!link) return link;
   return new URL(link, "https://extensions.gnome.org").href;
@@ -130,69 +68,15 @@ export const normalizeExtension = (extension: Extension): Extension => {
   };
 };
 
-export const extension = normalizeExtension(sampleExtension);
-
-const comments: Comment[] = [{
-  "gravatar":
-    "https://secure.gravatar.com/avatar/f2efecf11b2345dfb4ef5627e1b3430b?d=mm&s=70",
-  "is_extension_creator": false,
-  "comment":
-    "<p>Working on Pop!_OS 22.04 The flatpak, available in the Pop!_Shop doesn&#x27;t even launch!</p>",
-  "author": {
-    "username": "Islamic.Audiobooks.Central",
-    "url": "/accounts/profile/Islamic.Audiobooks.Central",
-  },
-  "date": { "timestamp": "2022-07-13T11:18:53", "standard": "July 13, 2022" },
-  "rating": 5,
-}, {
-  "gravatar":
-    "https://secure.gravatar.com/avatar/b7bac390764b8567fee99550d713f89f?d=mm&s=70",
-  "is_extension_creator": false,
-  "comment": "<p>Super useful utility!</p>",
-  "author": {
-    "username": "theCalcaholic",
-    "url": "/accounts/profile/theCalcaholic",
-  },
-  "date": { "timestamp": "2022-07-11T17:19:07", "standard": "July 11, 2022" },
-  "rating": 4,
-}, {
-  "gravatar":
-    "https://secure.gravatar.com/avatar/81a5c34db4a6df4cf6784136805b30d6?d=mm&s=70",
-  "is_extension_creator": false,
-  "comment":
-    "<p>Hi,<br>On May 21, 2022, I posted that the SFTP mount is not working for Gnome 42 Fedora 36. The situation is still the same. Please let me if you need any more inputs to debug.<br>Thanks,<br>Mallikarjun</p>",
-  "author": { "username": "ms.patil", "url": "/accounts/profile/ms.patil" },
-  "date": { "timestamp": "2022-06-28T11:50:49", "standard": "June 28, 2022" },
-}, {
-  "gravatar":
-    "https://secure.gravatar.com/avatar/ad06e69f6525f4a2f88d6c93efde970f?d=mm&s=70",
-  "is_extension_creator": false,
-  "comment":
-    "<p>When I really need it always fails, can&#x27;t send files, can&#x27;t mount. Only work to see my phone notifications. I don&#x27;t have time to see what happens, searching in forums, etc.</p>",
-  "author": { "username": "Madbyte", "url": "/accounts/profile/Madbyte" },
-  "date": { "timestamp": "2022-06-23T13:23:51", "standard": "June 23, 2022" },
-  "rating": 1,
-}, {
-  "gravatar":
-    "https://secure.gravatar.com/avatar/f85bf3f6fc0b0623dcfa2da111163def?d=mm&s=70",
-  "is_extension_creator": false,
-  "comment":
-    "<p>Great extension, but I don&#x27;t know why it floods my log files until my disk is out of space, in Ubuntu 20.04.3 :(</p>",
-  "author": {
-    "username": "argentina2021",
-    "url": "/accounts/profile/argentina2021",
-  },
-  "date": { "timestamp": "2022-06-22T12:59:42", "standard": "June 22, 2022" },
-  "rating": 4,
-}];
-
-export const sampleComments = comments.map(comment => {
-  comment.author = {
-    ...comment.author,
-    url: normalizeLink(comment.author.url),
+export const normalizeComment = (comment: Comment): Comment => {
+  return {
+    ...comment,
+    author: {
+      ...comment.author,
+      url: normalizeLink(comment.author.url),
+    }
   };
-  return comment;
-})
+}
 
 const load = async (url: string): Promise<any> => {
   url = proxy(url);
@@ -229,3 +113,4 @@ export const getComments = (primaryKey: number, loadAll = false): Promise<Commen
 export const getExtension = (uuid: string): Promise<Extension | null> => {
   return load(`/extension-info/?uuid=${uuid}`);
 }
+
